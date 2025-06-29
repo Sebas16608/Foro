79faf61ac3718b7ae86e9da90f327ff03319fe10
Sebas16608 <asebasrr444@gmail.com>
2025-06-29 16:45:13 -0600

    Agregando los base.html a cada app tambien añadiendo cada url de cada app

--- foro/foro/settings.py
+++ foro/foro/settings.py
@@ -130,6 +130,23 @@ STATICFILES_DIR = [
     'manage_post/static',
     'user/static',
 ]
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'], 
+        'APP_DIRS': True,                 
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- foro/foro/urls.py
+++ foro/foro/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('boards/', include('boards.urls')),
+    path('threads/', include('threads.urls'))
+    path('posts/', include('posts.urls'))
 ]
